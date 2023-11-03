408fbc10f9aa6f94e03a0f9c8bc51a6311edb2b3
DipuBarua <you@example.com>
2023-11-03 23:05:09 +0600

    sent jwt-token to client side using cookie

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+}));
 app.use(express.json());
 
 
@@ -35,7 +38,14 @@ async function run() {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send(token);
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,//http://localhost:5173
+                    sameSite: 'none',
+                })
+                .send({ success: true });
         })
 
 
